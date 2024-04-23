ebf5d99bbe807a95974b15c9873275afa4b13010
Ivan Cristian Quevedo Burgos <ivanqb@ext.zara.com>
2024-04-23 16:25:53 +0200

    feat: Add upload form logic

---
+++ src/components/forms/CreateFeedForm.tsx
@@ -0,0 +1,88 @@
+import useToggle from "@/hooks/useToggle";
+
+const CreateFeedForm = () => {
+  const [isValidForm, setIsValidForm] = useToggle();
+  return (
+    <form
+      className="mt-8 space-y-3"
+      onSubmit={(e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target as HTMLFormElement);
+        // Upload data to service
+      }}
+    >
+      <div className="grid grid-cols-1 space-y-2">
+        <label className="text-sm font-bold text-gray-500 tracking-wide">
+          Title
+        </label>
+        <input
+          name="title"
+          className="text-base p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
+          type=""
+          placeholder="Image title"
+        />
+      </div>
+      <div className="grid grid-cols-1 space-y-2">
+        <label className="text-sm font-bold text-gray-500 tracking-wide">
+          Upload Image
+        </label>
+        <div className="flex items-center justify-center w-full">
+          <label className="flex flex-col rounded-lg border-4 border-dashed w-full h-60 p-10 group text-center">
+            <div className="h-full w-full text-center flex flex-col items-center justify-center items-center  ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-10 h-10 text-blue-400 group-hover:text-blue-600"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                />
+              </svg>
+              <div className="flex flex-auto max-h-48 w-2/5 mx-auto -mt-10">
+                <img
+                  className="has-mask h-36 object-center"
+                  src="https://img.freepik.com/free-vector/image-upload-concept-landing-page_52683-27130.jpg?size=338&ext=jpg"
+                  alt="freepik image"
+                />
+              </div>
+              <p className="pointer-none text-gray-500 ">
+                <span className="text-sm">Drag and drop</span> images here{" "}
+                <br /> or{" "}
+                <span className="text-blue-600 hover:underline cursor-pointer">
+                  select an image
+                </span>{" "}
+                from your computer
+              </p>
+            </div>
+            <input
+              name="image"
+              type="file"
+              accept="image/png, image/jpeg, image/jpg"
+              className="hidden"
+              onChange={() => setIsValidForm(true)}
+            />
+          </label>
+        </div>
+      </div>
+      <p className="text-sm text-gray-300">
+        <span>Image type: jpg, jpeg, png</span>
+      </p>
+      <div className="flex justify-end">
+        <button
+          disabled={!isValidForm}
+          type="submit"
+          className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:w-auto sm:text-sm"
+        >
+          Upload
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default CreateFeedForm;
--- src/components/modals/CreateFeedModal.tsx
+++ src/components/modals/CreateFeedModal.tsx
@@ -1,3 +1,5 @@
+import CreateFeedForm from "@/components/forms/CreateFeedForm";
+
 const CreateFeedModal = ({ onClose }: { onClose: () => void }) => {
   return (
     <div className="fixed z-50 inset-0 overflow-y-auto">
@@ -47,38 +49,7 @@ const CreateFeedModal = ({ onClose }: { onClose: () => void }) => {
               </svg>
             </button>
           </div>
-          <div className="sm:flex sm:items-start">
-            <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-              <h3
-                className="text-lg leading-6 font-medium text-gray-900"
-                id="modal-headline"
-              >
-                Your Confirmation Message
-              </h3>
-              <div className="mt-2">
-                <p className="text-sm text-gray-500">
-                  Your body text goes here.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-            <button
-              type="button"
-              data-behavior="commit"
-              className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
-            >
-              Commit
-            </button>
-            <button
-              type="button"
-              data-behavior="cancel"
-              className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:w-auto sm:text-sm"
-              onClick={() => onClose()}
-            >
-              Cancel
-            </button>
-          </div>
+          <CreateFeedForm />
         </div>
       </div>
     </div>
