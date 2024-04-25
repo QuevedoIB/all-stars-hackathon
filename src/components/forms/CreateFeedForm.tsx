3194374c85c5909dd3c6d5a4ec139dd10f78f288
Ivan Cristian Quevedo Burgos <ivanqb@ext.zara.com>
2024-04-25 16:27:50 +0200

    style: Remove darkmode styles

--- src/components/forms/CreateFeedForm.tsx
+++ src/components/forms/CreateFeedForm.tsx
@@ -102,7 +102,7 @@ const CreateFeedForm = ({ onClose }: { onClose: () => void }) => {
         <textarea
           id="caption"
           rows={4}
-          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:outline-none focus:border-indigo-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:outline-none focus:border-indigo-500"
           placeholder="Write your thoughts here..."
         ></textarea>
       </div>
