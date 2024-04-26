05e7885ad9558d32a435b7fa5005b0ba30322607
Ivan Cristian Quevedo Burgos <ivanqb@ext.zara.com>
2024-04-26 12:59:46 +0200

    style: Reuse styles

--- src/components/lists/StatsList.tsx
+++ src/components/lists/StatsList.tsx
@@ -1,3 +1,62 @@
+import { PropsWithChildren } from "react";
+
+const ArrowUp = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      fillRule="evenodd"
+      d="M15.22 6.268a.75.75 0 0 1 .968-.431l5.942 2.28a.75.75 0 0 1 .431.97l-2.28 5.94a.75.75 0 1 1-1.4-.537l1.63-4.251-1.086.484a11.2 11.2 0 0 0-5.45 5.173.75.75 0 0 1-1.199.19L9 12.312l-6.22 6.22a.75.75 0 0 1-1.06-1.061l6.75-6.75a.75.75 0 0 1 1.06 0l3.606 3.606a12.695 12.695 0 0 1 5.68-4.974l1.086-.483-4.251-1.632a.75.75 0 0 1-.432-.97Z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+const ArrowDown = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      fillRule="evenodd"
+      d="M1.72 5.47a.75.75 0 0 1 1.06 0L9 11.69l3.756-3.756a.75.75 0 0 1 .985-.066 12.698 12.698 0 0 1 4.575 6.832l.308 1.149 2.277-3.943a.75.75 0 1 1 1.299.75l-3.182 5.51a.75.75 0 0 1-1.025.275l-5.511-3.181a.75.75 0 0 1 .75-1.3l3.943 2.277-.308-1.149a11.194 11.194 0 0 0-3.528-5.617l-3.809 3.81a.75.75 0 0 1-1.06 0L1.72 6.53a.75.75 0 0 1 0-1.061Z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+const MainIconContainer = ({ children }: PropsWithChildren) => (
+  <div className="w-fit	rounded-full bg-gradient-to-tr from-pink-300 to-blue-300 p-0.5 shadow-lg">
+    <div className="bg-white text-blue-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 rounded-full">
+      {children}
+    </div>
+  </div>
+);
+
+const FooterText = ({ value, unit }: { value: number; unit?: string }) => {
+  const hasIncreased = value > 0;
+  return (
+    <p className="flex text-sm text-blueGray-400 mt-4">
+      <span
+        className={`${
+          hasIncreased ? "text-emerald-500" : "text-red-500"
+        } flex mr-2`}
+      >
+        {hasIncreased ? <ArrowUp /> : <ArrowDown />}{" "}
+        <span className="ml-1">
+          {Math.abs(value)} {unit}
+        </span>
+      </span>
+      <span className="whitespace-nowrap"> Since last month </span>
+    </p>
+  );
+};
+
 const StatsList = () => {
   return (
     <div className="flex flex-wrap">
@@ -11,21 +70,28 @@ const StatsList = () => {
                   Views
                 </h5>
                 <span className="font-semibold text-xl text-blueGray-700">
-                  4387
+                  4.387
                 </span>
               </div>
               <div className="relative w-auto pl-4 flex-initial">
-                <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full  bg-red-500">
-                  <i className="fas fa-chart-bar"></i>
-                </div>
+                <MainIconContainer>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+                    <path
+                      fillRule="evenodd"
+                      d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </MainIconContainer>
               </div>
             </div>
-            <p className="text-sm text-blueGray-400 mt-4">
-              <span className="text-emerald-500 mr-2">
-                <i className="fas fa-arrow-up"></i> +2,99%{" "}
-              </span>
-              <span className="whitespace-nowrap"> Since last month </span>
-            </p>
+            <FooterText value={2.99} unit="%" />
           </div>
         </div>
       </div>
@@ -39,21 +105,23 @@ const StatsList = () => {
                   Likes
                 </h5>
                 <span className="font-semibold text-xl text-blueGray-700">
-                  2,999
+                  3.124
                 </span>
               </div>
               <div className="relative w-auto pl-4 flex-initial">
-                <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full  bg-pink-500">
-                  <i className="fas fa-chart-pie"></i>
-                </div>
+                <MainIconContainer>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+                  </svg>
+                </MainIconContainer>
               </div>
             </div>
-            <p className="text-sm text-blueGray-400 mt-4">
-              <span className="text-red-500 mr-2">
-                <i className="fas fa-arrow-down"></i> 4,01%
-              </span>
-              <span className="whitespace-nowrap"> Since last month </span>
-            </p>
+            <FooterText value={4.01} unit="%" />
           </div>
         </div>
       </div>
@@ -67,21 +135,23 @@ const StatsList = () => {
                   Passes
                 </h5>
                 <span className="font-semibold text-xl text-blueGray-700">
-                  901
+                  1.263
                 </span>
               </div>
               <div className="relative w-auto pl-4 flex-initial">
-                <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full  bg-lightBlue-500">
-                  <i className="fas fa-users"></i>
-                </div>
+                <MainIconContainer>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path d="M15.73 5.5h1.035A7.465 7.465 0 0 1 18 9.625a7.465 7.465 0 0 1-1.235 4.125h-.148c-.806 0-1.534.446-2.031 1.08a9.04 9.04 0 0 1-2.861 2.4c-.723.384-1.35.956-1.653 1.715a4.499 4.499 0 0 0-.322 1.672v.633A.75.75 0 0 1 9 22a2.25 2.25 0 0 1-2.25-2.25c0-1.152.26-2.243.723-3.218.266-.558-.107-1.282-.725-1.282H3.622c-1.026 0-1.945-.694-2.054-1.715A12.137 12.137 0 0 1 1.5 12.25c0-2.848.992-5.464 2.649-7.521C4.537 4.247 5.136 4 5.754 4H9.77a4.5 4.5 0 0 1 1.423.23l3.114 1.04a4.5 4.5 0 0 0 1.423.23ZM21.669 14.023c.536-1.362.831-2.845.831-4.398 0-1.22-.182-2.398-.52-3.507-.26-.85-1.084-1.368-1.973-1.368H19.1c-.445 0-.72.498-.523.898.591 1.2.924 2.55.924 3.977a8.958 8.958 0 0 1-1.302 4.666c-.245.403.028.959.5.959h1.053c.832 0 1.612-.453 1.918-1.227Z" />
+                  </svg>
+                </MainIconContainer>
               </div>
             </div>
-            <p className="text-sm text-blueGray-400 mt-4">
-              <span className="text-red-500 mr-2">
-                <i className="fas fa-arrow-down"></i> 1,25%{" "}
-              </span>
-              <span className="whitespace-nowrap"> Since last month </span>
-            </p>
+            <FooterText value={-1.25} unit="%" />
           </div>
         </div>
       </div>
@@ -92,24 +162,26 @@ const StatsList = () => {
             <div className="flex flex-wrap">
               <div className="relative w-full pr-4 max-w-full flex-grow flex-1">
                 <h5 className="text-blueGray-400 uppercase font-bold text-xs">
-                  Average
+                  Average likes
                 </h5>
                 <span className="font-semibold text-xl text-blueGray-700">
-                  51.02%{" "}
+                  71.21%{" "}
                 </span>
               </div>
               <div className="relative w-auto pl-4 flex-initial">
-                <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full  bg-emerald-500">
-                  <i className="fas fa-percent"></i>
-                </div>
+                <MainIconContainer>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75ZM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 0 1-1.875-1.875V8.625ZM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 0 1 3 19.875v-6.75Z" />
+                  </svg>
+                </MainIconContainer>
               </div>
             </div>
-            <p className="text-sm text-blueGray-400 mt-4">
-              <span className="text-emerald-500 mr-2">
-                <i className="fas fa-arrow-up"></i> 12%{" "}
-              </span>
-              <span className="whitespace-nowrap"> Since last mounth </span>
-            </p>
+            <FooterText value={5.2} unit="%" />
           </div>
         </div>
       </div>
